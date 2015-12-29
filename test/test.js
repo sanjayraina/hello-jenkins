71943afa82f47ff5c264a918badf473fa410b3f2
Sanjay Raina <sanjay.raina@optumis.com>
2015-12-29 16:34:06 +0000

    Change test to hello from jenkins

--- test/test.js
+++ test/test.js
@@ -3,13 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
    it('respond with hello world', function(done) {
-      request(app).get('/').expect('hello world', done);
-   });
-});var request = require('supertest');
-var app = require('../app.js');
-
-describe('GET /', function() {
-   it('respond with hello world', function(done) {
-      request(app).get('/').expect('hello world', done);
+      request(app).get('/').expect('hello from jenkins', done);
    });
 });
